3d7fd87bdbe2437125ec6322afd06bfeedeba65d
SilvaEmerson <emersonjose877@gmail.com>
2018-02-04 13:00:13 -0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { RestProvider } from '../providers/rest/rest';
+import { Camera } from '@ionic-native/camera';
 
 @NgModule({
   declarations: [
@@ -13,6 +16,7 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
@@ -23,8 +27,10 @@ import { HomePage } from '../pages/home/home';
   ],
   providers: [
     StatusBar,
+    Camera,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    RestProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,61 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import { RestProvider } from "../../providers/rest/rest";
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  
+  private image: string;
+  private response: any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+              public restProvider: RestProvider,
+              public alertCtrl: AlertController,
+              public camera: Camera,
+              public domSanitizer: DomSanitizer) {}
 
+  getPerson() {
+    console.log(typeof this.image);
+
+    this.restProvider.getPerson(this.image)
+      .then(data => {
+        console.log(data);
+        this.response = data;
+      })
+      .catch(err => {
+        this.displayErrorAlert(err.message)
+      })
   }
 
+  onTakePicture() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      saveToPhotoAlbum: true,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.image = 'data:image/jpeg;base64,' + imageData;
+      console.log(this.domSanitizer.bypassSecurityTrustUrl(imageData)['changingThisBreaksApplicationSecurity']);
+    }, (err) => {
+      this.displayErrorAlert(err.message);
+    });
+  }
+
+  displayErrorAlert(error) {
+    let alert = this.alertCtrl.create({
+      title: 'Error',
+      subTitle: error,
+      buttons: ['OK']
+    });
+
+    alert.present();
+  }
 }
