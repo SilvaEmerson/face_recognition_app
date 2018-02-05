b28ece66452396a46283dc8b1d66accd2f18b62f
SilvaEmerson <emersonjose877@gmail.com>
2018-02-04 23:00:42 -0300

    Add gallery acess

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -27,10 +27,14 @@ export class HomePage {
     console.log(typeof this.image);
 
     let formData: FormData = new FormData();
-    formData.append('file', this.image);
+    formData.append('file', this.image, 'submition.jpeg');
 
     this.http.post('https://face-cognition.herokuapp.com/', formData,
-                  {responseType: "text"})
+                  {headers: {
+                    'Content-Type': 'multipart/form-data',
+                    
+                  },
+                  responseType: "text"})
         .subscribe(res =>{
           this.response = res;
         }, err => {
@@ -50,12 +54,34 @@ export class HomePage {
     const options: CameraOptions = {
       quality: 100,
       destinationType: this.camera.DestinationType.DATA_URL,
-      saveToPhotoAlbum: true,
-      mediaType: this.camera.MediaType.PICTURE
+      saveToPhotoAlbum: false,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      sourceType: this.camera.PictureSourceType.CAMERA,
     }
 
     this.camera.getPicture(options).then((imageData) => {
-      this.image = 'data:image/jpeg;base64,' + imageData;
+      // this.image = 'data:image/jpeg;base64,' + imageData;
+      this.image = imageData;
+      console.log(this.domSanitizer.bypassSecurityTrustUrl(imageData)['changingThisBreaksApplicationSecurity']);
+    }, (err) => {
+      this.displayErrorAlert(err.message);
+    });
+  }
+
+  onChooseImageFromGallery() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      saveToPhotoAlbum: false,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      // this.image = 'data:image/jpeg;base64,' + imageData;
+      this.image = this.domSanitizer.bypassSecurityTrustUrl(imageData)['changingThisBreaksApplicationSecurity'];
       console.log(this.domSanitizer.bypassSecurityTrustUrl(imageData)['changingThisBreaksApplicationSecurity']);
     }, (err) => {
       this.displayErrorAlert(err.message);
